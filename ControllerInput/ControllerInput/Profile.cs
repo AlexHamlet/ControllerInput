a0ac5b129235d71a87b6340a986ea93e2324b649
Alex Hamlet <alexhamlet@comcast.net>
2019-12-26 23:07:35 -0700

    Idea for the input profiles added.

---
+++ ControllerInput/ControllerInput/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerInput
+{
+    class Profile
+    {
+        //This class is going to store input profiles
+        //Default profile should be included, additional profiles may be created by the user
+        //this should be referenced when keyboard/mouse input is required
+    }
+}

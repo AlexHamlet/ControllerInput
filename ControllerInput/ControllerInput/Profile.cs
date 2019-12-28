47dd254f6d290093b7b2a0a40751992e2595640a
Alex Hamlet <alexhamlet@comcast.net>
2019-12-27 22:45:09 -0700

    Outlined a few Profile methods

--- ControllerInput/ControllerInput/Profile.cs
+++ ControllerInput/ControllerInput/Profile.cs
@@ -11,5 +11,26 @@ namespace ControllerInput
         //This class is going to store input profiles
         //Default profile should be included, additional profiles may be created by the user
         //this should be referenced when keyboard/mouse input is required
+        public Profile selected { get; private set; }
+        
+        public static List<Profile> getProfiles()
+        {
+            return null;
+        }
+
+        public void setProfile()
+        {
+
+        }
+
+        public void setButton()
+        {
+
+        }
+
+        public void getButton()
+        {
+
+        }
     }
 }
